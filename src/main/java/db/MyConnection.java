7ed90eac91746f529d43bdd253854adc63b00418
abmamun504@gmail.com <abmamun504@gmail.com>
2024-05-20 00:28:59 -0400

    setting up project and connected with database

---
+++ src/main/java/db/MyConnection.java
@@ -0,0 +1,32 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyConnection {
+    public static Connection connection = null;
+    public static Connection getConnection(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/fileLocker?useSSl = false","root","mamun@504");
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("successfully connected!");
+        return connection;
+    }
+    public static void closeConnection(){
+        if(connection != null){
+            try {
+                connection.close();
+            }catch (SQLException ex){
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        MyConnection.getConnection();
+    }
+}
