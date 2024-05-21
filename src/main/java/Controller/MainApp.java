5c5ed783e238470c706a412b9c96076c3a3f9478
abmamun504@gmail.com <abmamun504@gmail.com>
2024-05-21 04:48:22 -0400

    Main app with gui

--- src/main/java/Controller/MainApp.java
+++ src/main/java/Controller/MainApp.java
@@ -1,15 +1,15 @@
 package Controller;
-import views.LoginSignUpView;
 
-import javax.swing.*;
+
+import javax.swing.SwingUtilities;
 
 public class MainApp {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            LoginSignUpView view = new LoginSignUpView();
-            new LoginSignUpController(view);
+
+            WelcomeView view = new WelcomeView();
+            new WelcomeController(view);
             view.setVisible(true);
         });
     }
 }
-
